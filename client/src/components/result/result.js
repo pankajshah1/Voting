85e7a280f102fe23f501c3900d8e8ed7c809bbce
Pankaj Shah <pankaj.073bct550@acem.edu.np>
2021-02-21 16:25:57 +0545

    updated result

--- client/src/components/result/result.js
+++ client/src/components/result/result.js
@@ -5,9 +5,33 @@ import decode from "jwt-decode";
 import Chart from "chart.js";
 import { Pie, Doughnut } from "react-chartjs-2";
 
+function Winnerfunc(props) {
 
-class Result extends Component{
- state = {
+  const image = `uploads/${props.winner[0].citizenship}.jpg`
+          return (
+            <div className="col-sm" style={{ marginBottom: "20px" }}>
+              <div className="card" style={{ width: "18rem" }}>
+                <img
+                  className="card-img-top"
+                  src={image}
+                  alt="Card image cap"
+                  width="100px"
+                  height="150px"
+                />
+                <div className="card-body">
+                  <p className="card-text">{props.winner[0].name}</p>
+                  <p className="card-text">Party: {props.winner[0].party}</p>
+                  <p className="card-text">Votes : {props.winner[0].votes} (winner)</p>
+                 
+                </div>
+              </div>
+            </div>
+          );
+  
+}
+
+class Result extends Component {
+  state = {
     storageValue: 0,
     web3: null,
     accounts: null,
@@ -19,11 +43,12 @@ class Result extends Component{
     totalCandidate: null,
     candidates: [],
     candidateVotes: [],
-   winner: "",
+    winner: "",
+    winnerObject: [],
   };
 
-   componentDidMount = async () => {
-   // this.setState({storageValue:20 , web3});
+  componentDidMount = async () => {
+    // this.setState({storageValue:20 , web3});
     const web3 = await getWeb3();
     try {
       const accounts = await web3.eth.getAccounts();
@@ -38,7 +63,6 @@ class Result extends Component{
       const voter = await instance.methods.totalVoter.call().call();
       const candidate = await instance.methods.totalCandidate.call().call();
       const win = await instance.methods.winner.call().call();
-      
 
       this.setState({
         voteStatus: sta,
@@ -52,62 +76,55 @@ class Result extends Component{
         storageValue: 20,
         winner: win,
         candidatesInfo: [],
-        winnerObject:[],
       });
       for (let i = 0; i <= this.state.totalCandidate - 1; i++) {
         const data = await this.state.contract.methods
           .candidateAddress(i)
           .call();
-        const dataa = await this.state.contract.methods.candRegister(data).call();
-        
+        const dataa = await this.state.contract.methods
+          .candRegister(data)
+          .call();
+
         this.state.candidates.push(data);
         this.state.candidatesInfo.push(dataa);
       }
-   
-      this.state.candidates.forEach(async (value) => {
-        let votes = await this.state.contract.methods.votesRecieved(value).call();
-        this.state.candidateVotes.push(parseInt(votes));
 
-      
+      this.state.candidates.forEach(async (value) => {
+        let votes = await this.state.contract.methods
+          .votesRecieved(value)
+          .call();
+        this.state.candidateVotes.push(parseInt(votes));
       });
- 
+
       let winner_var = this.state.winner;
-      console.log(winner_var)
-      var winnerObject_var =  this.state.candidatesInfo.filter(function(obs) {
-         return obs.candAddress == winner_var;
+      console.log(winner_var);
+      var winnerObject_var = this.state.candidatesInfo.filter(function (obs) {
+        return obs.candAddress == winner_var;
       });
       this.state.winnerObject = winnerObject_var;
       console.log(this.state.candidatesInfo);
       console.log(this.state.winnerObject[0].citizenship);
-    
     } catch (error) {
       console.error(error);
     }
+
+    this.setState(this.state);
   };
 
   resultDisplay = () => {
-     
-      console.log(this.state.candidates);
+    console.log(this.state.candidates);
 
     console.log(this.state.candidateVotes);
-    
-    
-  }
+  };
 
-
- 
   render() {
+    console.log(this.state.winnerObject);
 
     let display;
     if (this.state.voteStatus == 0) {
       display = <div> vote has not started yet</div>;
     } else if (this.state.voteStatus == 1) {
-      display = (
-        <div>
-          {" "}
-        vote is running
-        </div>
-      );
+      display = <div> vote is running</div>;
       console.log(this.state.candidateVotes);
     } else {
       display = (
@@ -141,7 +158,7 @@ class Result extends Component{
               options={{
                 title: {
                   display: true,
-                  text: "Average Rainfall per month",
+                  text: "Voting Result",
                   fontSize: 10,
                 },
                 legend: {
@@ -151,25 +168,20 @@ class Result extends Component{
               }}
             />
           </div>
-          <div style={{ height: "400px", width: "20vw", float: "right" }}>
+          <div style={{ height: "400px", width: "20vw", float: "right" , marginTop:"50px" }}>
             <div>
-                
-            </div>
-            <div>
-
-            {this.state.winner}
+              {this.state.winnerObject.length == 0 ? (
+                <div>hello</div>
+              ) : (
+                <Winnerfunc winner={this.state.winnerObject} />
+              )}
             </div>
           </div>
         </div>
       );
     }
-    
-    
-    return (
-      <div>
-        {display}
-      </div>
-    );
+
+    return <div>{display}</div>;
   }
 }
 
