48af2d545ec4a07699b1c5a967cbb83777b1bb43
Pankaj Shah <pankaj.073bct550@acem.edu.np>
2021-02-21 14:11:03 +0545

    result updated

--- client/src/components/result/result.js
+++ client/src/components/result/result.js
@@ -2,6 +2,9 @@ import React ,{useState , useEffect, Component} from "react";
 import VotingContract from "../../contracts/Voting.json";
 import getWeb3 from "../../getWeb3";
 import decode from "jwt-decode";
+import Chart from "chart.js";
+import { Pie, Doughnut } from "react-chartjs-2";
+
 
 class Result extends Component{
  state = {
@@ -16,7 +19,7 @@ class Result extends Component{
     totalCandidate: null,
     candidates: [],
     candidateVotes: [],
-    name:"",
+   winner: "",
   };
 
    componentDidMount = async () => {
@@ -46,7 +49,6 @@ class Result extends Component{
         totalCandidate: candidate,
         storageValue: 20,
       });
-      console.log("hello");
       for (let i = 0; i <= this.state.totalCandidate - 1; i++) {
         const data = await this.state.contract.methods
           .candidateAddress(i)
@@ -56,30 +58,93 @@ class Result extends Component{
    
       this.state.candidates.forEach(async (value) => {
         let votes = await this.state.contract.methods.votesRecieved(value).call();
-        this.state.candidateVotes.push(votes);
+        this.state.candidateVotes.push(parseInt("10"));
 
       
       });
-      if (this.state.candidateVotes.length !== 0) {
-        
-        console.log(this.state.candidateVotes);
-      }
-      console.log(this.state.candidates);
-      // console.log(this.state.candidateVotes);
+      console.log(this.state.candidateVotes.length);
+    
     } catch (error) {
       console.error(error);
     }
   };
 
+  resultDisplay = () => {
+     
+      console.log(this.state.candidates);
+
+    console.log(this.state.candidateVotes);
+    
+    
+  }
+
 
  
   render() {
+
+    let display;
+    if (this.state.voteStatus == 0) {
+      display = <div> vote has not started yet</div>;
+    } else if (this.state.voteStatus == 1) {
+      display = (
+        <div>
+          {" "}
+          <button onClick={this.resultDisplay}>click</button>
+          <div style={{height:"400px" , width:"75vw"}}>
+            <Pie
+              data={{
+                labels: this.state.candidates,
+                datasets: [
+                  {
+                    label: "Rainfall",
+                    backgroundColor: [
+                      "#B21F00",
+                      "#C9DE00",
+                      "#2FDE00",
+                      "#00A6B4",
+                      "#6800B4",
+                    ],
+                    hoverBackgroundColor: [
+                      "#501800",
+                      "#4B5000",
+                      "#175000",
+                      "#003350",
+                      "#35014F",
+                    ],
+                    data: this.state.candidateVotes,
+                  },
+                ],
+              }}
+              options={{
+                title: {
+                  display: true,
+                  text: "Average Rainfall per month",
+                  fontSize: 10,
+                },
+                legend: {
+                  display: true,
+                  position: "bottom",
+                },
+              }}
+            />
+          </div>
+        </div>
+      );
+      console.log(this.state.candidateVotes);
+    } else {
+      display = (
+        <div>
+        </div>
+      );
+    }
+    
     
     return (
       <div>
-            hello
-        </div>
-    )
+        {display}
+
+      </div>
+    );
   }
 }
 
