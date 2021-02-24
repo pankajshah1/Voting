05b1e8e7a35fa6a45de028ea141c101bbbaa460c
Pankaj Shah <pankaj.073bct550@acem.edu.np>
2021-02-24 08:34:27 +0545

    updated landing page

--- client/src/components/landing/landing.js
+++ client/src/components/landing/landing.js
@@ -114,11 +114,85 @@ function Contact() {
 const Landing = (props) => {
   return (
     <div className={classes.container}>
-      <Header />
-      <Intro />
-      <Buttons props={props} />
-      {/* <Contact /> */}
+      <section className={classes.glass}>
+        <div className={classes.dashboard}>
+          <div
+            className={classes.logo}
+            onClick={() => props.history.push("/login")}
+          >
+            <img src="./images/voting-box.png" alt="" />
+            <h3>Evoting</h3>
+          </div>
+          <div className={classes.links}>
+            <div className={classes.link}>
+              <h2>Home</h2>
+            </div>
+            <div className={classes.link}>
+              <h2>Menu</h2>
+            </div>
+            <div className={classes.link}>
+              <h2>Help</h2>
+            </div>
+            <div className={classes.link}>
+              <h2>Support</h2>
+            </div>
+          </div>
+          <div className={classes.pro}>
+            <h2>Join to support your parties</h2>
+            <img src="./images/political-party.png" alt="" />
+          </div>
+        </div>
+        <div className={classes.right}>
+          <div className={classes.status}>
+            <h1>
+              <strong>Your Voice Your Vote</strong>
+            </h1>
+          </div>
+          <div className={classes.cards}>
+            <div
+              className={classes.card}
+              onClick={() => props.history.push("/voter")}
+            >
+              <img src="./images/ballot.png" alt="" />
+              <div className={classes.cardInfo}>
+                <h2>Apply for Voter</h2>
+              </div>
+            </div>
+
+            <div
+              className={classes.card}
+              onClick={() => props.history.push("/candidate")}
+            >
+              <img src="./images/employee.png" alt="" />
+              <div className={classes.cardInfo}>
+                <h2>Apply for Candidate</h2>
+              </div>
+            </div>
+            <div
+              className={classes.card}
+              onClick={() => props.history.push("/result")}
+            >
+              <img src="./images/stadistics.png" alt="" />
+              <div className={classes.cardInfo}>
+                <h2>Show results</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
 export default Landing;
+
+
+
+
+
+
+//  <div className={classes.container}>
+//       <Header />
+//       <Intro />
+//       <Buttons props={props} />
+//       {/* <Contact /> */}
+//     </div>
